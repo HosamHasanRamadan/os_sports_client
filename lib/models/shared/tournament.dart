2beb91caf16ddfc5841c1fbc25e8481f6530d0eb
HosamNovela <hosam@novelaneuro.com>
2022-06-26 18:52:41 +0200

    add touranment to shared

---
+++ lib/models/shared/tournament.dart
@@ -0,0 +1,32 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'package:os_sports_client/models/shared/category.dart';
+
+import 'package:os_sports_client/models/shared/unique_tournament.dart';
+
+part 'tournament.g.dart';
+
+@JsonSerializable()
+class Tournament {
+  final String? name;
+  final String? slug;
+  final Category? category;
+  final UniqueTournament? uniqueTournament;
+  final int? priority;
+  final int? id;
+
+  const Tournament({
+    this.name,
+    this.slug,
+    this.category,
+    this.uniqueTournament,
+    this.priority,
+    this.id,
+  });
+
+  factory Tournament.fromJson(Map<String, dynamic> json) {
+    return _$TournamentFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$TournamentToJson(this);
+}
